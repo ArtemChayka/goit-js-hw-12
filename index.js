4d0d5b35115787667cf20daf90bbf7365bf0158c
ArtemChayka <drchaychenko@gmail.com>
2025-06-22 22:02:13 +0000

    Deploying to gh-pages from @ ArtemChayka/goit-js-hw-12@56c29179e66698b3b4a2f28127e9f752c796e229 🚀

--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-/* empty css                      */(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&i(o)}).observe(document,{childList:!0,subtree:!0});function s(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerPolicy&&(r.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?r.credentials="include":e.crossOrigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function i(e){if(e.ep)return;e.ep=!0;const r=s(e);fetch(e.href,r)}})();
+import{a as c,S as l}from"./assets/vendor-B6Fq-XDb.js";(function(){const o=document.createElement("link").relList;if(o&&o.supports&&o.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const n of t.addedNodes)n.tagName==="LINK"&&n.rel==="modulepreload"&&s(n)}).observe(document,{childList:!0,subtree:!0});function i(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?t.credentials="include":e.crossOrigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function s(e){if(e.ep)return;e.ep=!0;const t=i(e);fetch(e.href,t)}})();const p="50832143-3e18ca1c7d3ff8d3379931b93",u="https://pixabay.com/api/";async function f(r,o){const i={key:p,q:r,image_type:"photo",orientation:"horizontal",safesearch:!0,per_page:15,page:1},{data:s}=await c(u,{params:i});return s.hits}function d(r){return r.map(({webformatURL:o,largeImageURL:i,tags:s,likes:e,views:t,comments:n,downloads:a})=>`
+<li class="gallery-item"><a href="${i}"><img src="${o}" alt="${s}">
+<div class="info">
+<p><b>Likes</b>${e}</p>
+<p><b>Views</b>${t}</p>
+<p><b>Comments</b>${n}</p>
+<p><b>Downloads</b>${a}</p>
+</div>
+</a></li>
+    `).join("")}const m=document.querySelector(".gallery");new l(".gallery a");document.querySelector(".load-btn");f().then(r=>{console.log(r),m.insertAdjacentHTML("beforeend",d(r))}).catch(r=>{console.log(r.message)});
 //# sourceMappingURL=index.js.map
